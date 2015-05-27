ede3e89c2c48e4d5d030547112c23a63dbfac4ff
caibo <464608293@qq.com>
2015-05-27 22:27:09 +0800

    添加getPath方法，获取打印模板文件的Windows绝对路径

--- IEPrinter/PrintTemplate.cpp
+++ IEPrinter/PrintTemplate.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.h"
+#include "resource.h"
 #include "PrintTemplate.h"
 
+string CPrintTemplate::_DEFAULT_PRINT_TEMPLATE_FILE_NAME = "PrintTemplate.htm";
 
 CPrintTemplate::CPrintTemplate()
 {
@@ -10,3 +12,9 @@ CPrintTemplate::CPrintTemplate()
 CPrintTemplate::~CPrintTemplate()
 {
 }
+
+string CPrintTemplate::getPath(string filename){
+	return "";
+}
+
+
